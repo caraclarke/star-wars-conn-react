bb4c9e3cee6c806453705326df66d2a875f57007
caraclarke <cara.clarke8@gmail.com>
2016-06-14 07:25:53 -0400

    Searchbar being imported for later use
    Searchbar imported to App for later use
    Not rendering on page yet
    Rendering test text to make sure ReactDOM working

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Searchbar from './components/search_bar';
+
+const App = () => {
+  return <div>test</div>
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+)
