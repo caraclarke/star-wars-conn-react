9be142d0ab2c4652e732c9c3ee9afe7deeb8220c
caraclarke <cara.clarke8@gmail.com>
2016-06-14 07:24:47 -0400

    webpack set up, added div for reactDOM to render to

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const DotenvPlugin = require('webpack-dotenv-plugin');
+
+module.exports = {
+  entry: [
+    './src/index.js'
+  ],
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  node: {
+       fs: "empty"
+   },
+  module: {
+    loaders: [{
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+        presets: ['react', 'es2015', 'stage-1']
+      }
+    }]
+  },
+  plugins: [
+    new DotenvPlugin({
+      path: './.env'
+    })
+  ],
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './'
+  }
+};
