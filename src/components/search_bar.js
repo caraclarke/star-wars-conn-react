3c329107006bb1b032a9b5ea4ef5b1e949ef362c
caraclarke <cara.clarke8@gmail.com>
2016-06-14 07:25:40 -0400

    Searchbar component set up and styling added

---
+++ src/components/search_bar.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Searchbar extends Component {
+
+  render() {
+    return (
+      <div className="search-bar">
+        <input />
+      </div>
+    );
+  }
+
+}
+
+export default Searchbar;
